0f525fdd6ace890a2f2a7fb4a17e15a858921b36
Matías Sandoval <101678664+Kodwell@users.noreply.github.com>
2022-12-01 00:36:53 -0300

    global config-instalacion karma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { IonicModule, isPlatform, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DbserviceService } from './services/dbservice.service';
@@ -11,9 +9,15 @@ import { MenuprincipalComponent } from './components/menuprincipal/menuprincipal
 
 @NgModule({
   declarations: [AppComponent, MenuprincipalComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Geolocation, SQLite,DbserviceService],
+  imports: [BrowserModule, IonicModule.forRoot({
+    //global config para app
+    rippleEffect: false,
+    mode: 'md'
+  }),
+  AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DbserviceService],
   exports:[MenuprincipalComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
