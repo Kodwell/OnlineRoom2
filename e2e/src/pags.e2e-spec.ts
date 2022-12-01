050f37d6a6711a2f8f3be49deca6787cb70ef27c
Matías Sandoval <101678664+Kodwell@users.noreply.github.com>
2022-12-01 20:14:12 -0300

    page test

--- e2e/src/pags.e2e-spec.ts
+++ e2e/src/pags.e2e-spec.ts
@@ -3,11 +3,23 @@ import { element, by, browser } from "protractor";
 describe('Test de login', ()=>{
     //codigo de config
     beforeEach(()=>{
-        browser.get("/");
+        browser.get("/home");
     });
     //prueba
     it ("La página debe entrar a login automaticamente",()=>{
-        expect(element(by.css(".page-selected ion-label")).getText()).toContain("/LoginPage");
+        expect(element(by.css(".page-selected ion-label")).getText()).toContain("/clases");
+        //expect indica que se debe hacer
+    });
+    it ("La página debe entrar a login automaticamente",()=>{
+        expect(element(by.css(".page-selected ion-label")).getText()).toContain("/calendario");
+        //expect indica que se debe hacer
+    });
+    it ("La página debe entrar a login automaticamente",()=>{
+        expect(element(by.css(".page-selected ion-label")).getText()).toContain("/notas");
+        //expect indica que se debe hacer
+    });
+    it ("La página debe entrar a login automaticamente",()=>{
+        expect(element(by.css(".page-selected ion-label")).getText()).toContain("/pagos");
         //expect indica que se debe hacer
     });
 });
